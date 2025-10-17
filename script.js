a33de9e17f16103b6893aa9b62b6ae639aaa4029
danishaeli <202404038@student.polytechnic.bh>
2025-10-17 17:31:42 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("contactForm").onsubmit=function () {alert("Your inquiry has been submitted.");
+                                                            return false;
+                 };
+
+
+document.getElementsByTagName("tr")[1].onclick=function(){alert("Sorry unavailable lol.");
+                           };
+
+
